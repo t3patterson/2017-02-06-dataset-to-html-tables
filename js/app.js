ebb89240eea25746ea98671c9c3de40e572ba4f7
Travis Hubbard <t3patterson@gmail.com>
2017-02-06 08:53:44 -0500

    files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------
+//TASK : 
+//   PART 1 -- Convert array of objects from `sample-data.js` 
+//             to HTML table rows (see index.html for example structure)
+//            
+//   PART 2 -- (ADVENTURE MODE!)
+//             Add an event listener to each remove icon button 
+//             that will remove the record from the data set and 
+//             render the new smaller data set back to the u.i.
+//-----------------------------------------------------------
